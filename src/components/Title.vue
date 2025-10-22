143bcfaa129bca78d24d35f230f2d502c0019b94
LeottaAlberto <leottaalberto43@gmail.com>
2025-10-22 12:10:22 +0200

    Title Component Created

---
+++ src/components/Title.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>
+            <h1>Viva Il cazzo</h1>
+            <input type="button" value="Add Activity" @click="addActivityClicked()">
+        </div>
+    </div>
+</template>
+
+<script setup>
+    function addActivityClicked(){
+        alert("aggingi activity");
+    }
+</script>
+
+<style scoped>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+  import TheWelcome from '../components/TheWelcome.vue'
+  import Title from '../components/Title.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Title />
+    <!-- <TheWelcome /> -->
   </main>
 </template>
