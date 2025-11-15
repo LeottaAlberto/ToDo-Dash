d504c0550e28c5eb567a64e2184051fdf56bc8af
LeottaAlberto <leottaalberto43@gmail.com>
2025-11-15 23:34:52 +0100

    feat(PopUpActivityComponents.vue): now the popup can be open and closed in correct way. If note message there isn't, show another message

--- src/components/PopUpActivityComponents.vue
+++ src/components/PopUpActivityComponents.vue
@@ -19,18 +19,24 @@ const props = defineProps<{
 watch(props, () => {
   console.log('PopUpSbloccato =>')
   console.log(props.activity)
-  isVisible.value = !isVisible.value
+  isVisible.value = true;
 })
+
+function closePopUp() {
+  isVisible.value = false;
+}
 </script>
 
 <template>
   <div class="w-50 flex pop-up-container" :class="{ visibility: !isVisible }">
+    <button @click="closePopUp()">X</button>
     <div class="w-100 flex" v-if="props.activity" style="flex-direction: column">
       <h1>{{ props.activity.title }}</h1>
       <h3>{{ props.activity.duration }}</h3>
       <h3>{{ props.activity.type }}</h3>
       <h3>{{ props.activity.priority }}</h3>
-      <h3>{{ props.activity.note }}</h3>
+      <h3 v-if="props.activity.note">{{ props.activity.note }}</h3>
+      <h3 v-else>Non ci sono note per questa Activity</h3>
     </div>
   </div>
 </template>
@@ -38,13 +44,22 @@ watch(props, () => {
 <style scoped>
 .pop-up-container {
   background-color: red;
-  min-width: 10vw;
+  width: 20vw;
   min-height: 10vw;
   position: absolute;
 }
 .pop-up-container div {
   position: absolute;
 }
+
+.pop-up-container button {
+  position: absolute;
+  right: 0.5vw;
+  top: 0.5vw;
+  background-color: blue;
+  z-index: 999;
+}
+
 .visibility {
   display: none;
 }
