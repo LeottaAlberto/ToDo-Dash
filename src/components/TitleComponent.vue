bb8784e938f01eca1d36f9b097ecafc49f4dc2ea
LeottaAlberto <leottaalberto43@gmail.com>
2025-12-06 20:50:18 +0100

    style(TitleComponent): applied automatic formatting

--- src/components/TitleComponent.vue
+++ src/components/TitleComponent.vue
@@ -2,46 +2,46 @@
   <div class="title-component-container flex">
     <div class="flex">
       <h1>My ToDo-Dashhhh</h1>
-      <input type="button" value="Add Activity" @click="addActivityClicked()" class="btn"/>
+      <input type="button" value="Add Activity" @click="addActivityClicked()" class="btn" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-  const emit = defineEmits(['clicked']);
+const emit = defineEmits(['clicked'])
 
-  function addActivityClicked() {
-    // alert('aggingi activity');
-    emit('clicked');
-  }
+function addActivityClicked() {
+  // alert('aggingi activity');
+  emit('clicked')
+}
 </script>
 
 <style scoped>
-  .title-component-container{
-    width: 80%;
-    height: 20%;
-    z-index: 1;
-  }
-  
-  .title-component-container div {
-    width: 100%;
-    height: max-content;
-    padding: 0.5vw;
-    padding-inline: 2vw;
-    flex-direction: row;
-    background-color: transparent;
-    border: 2px solid #4D4D4D;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    border-radius: 10px;
-  }
+.title-component-container {
+  width: 80%;
+  height: 20%;
+  z-index: 1;
+}
 
-  .btn {
-    width: 12%;
-    height: 4vh;
-    background-color: #6D6D6D;
-    font-size: large;
-    border-radius: 5px;
-  }
+.title-component-container div {
+  width: 100%;
+  height: max-content;
+  padding: 0.5vw;
+  padding-inline: 2vw;
+  flex-direction: row;
+  background-color: transparent;
+  border: 2px solid #4d4d4d;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 10px;
+}
+
+.btn {
+  width: 12%;
+  height: 4vh;
+  background-color: #6d6d6d;
+  font-size: large;
+  border-radius: 5px;
+}
 </style>
