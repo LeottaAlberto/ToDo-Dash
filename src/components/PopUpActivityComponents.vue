fede154f7f18f217e898359d7cca223d43130349
LeottaAlberto <leottaalberto43@gmail.com>
2025-12-06 22:51:26 +0100

    refactor(PopUpActivityComponent): rebuild popUp-body's html infrastructure

--- src/components/PopUpActivityComponents.vue
+++ src/components/PopUpActivityComponents.vue
@@ -22,25 +22,44 @@ function closePopUp() {
 <template>
   <div class="w-50 flex pop-up-container" :class="{ visibility: !isVisible }">
     <button @click="closePopUp()">X</button>
-    <div class="w-100 flex" v-if="props.activity" style="flex-direction: column">
-      <h1>{{ props.activity.title }}</h1>
-      <h3>{{ props.activity.duration }}</h3>
-      <h3>{{ props.activity.type }}</h3>
-      <h3>{{ props.activity.priority }}</h3>
-      <h3 v-if="props.activity.note">{{ props.activity.note }}</h3>
-      <h3 v-else>Non ci sono note per questa Activity</h3>
+    <div class="w-100 flex f-col pop-up-body g-2" v-if="props.activity">
+
+      <div class="flex flex-row w-100 text-align-start">
+        <h2 class="text-bolder font-size-big">Title {{ props.activity.title }}</h2>
+      </div>
+
+      <div class="flex flex-col w-100">
+
+        <div class="flex flex-row w-50 text-align-center">
+          <h1 class="text-bolder w-75">Duration = </h1>
+          <h2 class="w-25">{{ props.activity.duration }} h</h2>
+        </div>
+
+        <div class="flex flex-row w-50 text-align-center">
+          <h1 class="text-bolder w-75">Priority = </h1>
+          <h2 class="w-25">{{ props.activity.priority }}</h2>
+        </div>
+
+        <div class="flex flex-row w-50 text-align-center">
+          <h1 class="text-bolder w-75">Type = </h1>
+          <h2 class="w-25">{{ props.activity.type }}</h2>
+        </div>
+
+      </div>
+
+
+      <h2 v-if="props.activity.note">{{ props.activity.note }}</h2>
+      <h2 v-else>Non ci sono note per questa Activity</h2>
+
     </div>
   </div>
 </template>
 
 <style scoped>
 .pop-up-container {
-  background-color: red;
-  width: 20vw;
-  min-height: 10vw;
-  position: absolute;
-}
-.pop-up-container div {
+  background-color: grey;
+  width: 40vw;
+  min-height: 50vh;
   position: absolute;
 }
 
@@ -50,6 +69,11 @@ function closePopUp() {
   top: 0.5vw;
   background-color: blue;
   z-index: 999;
+}
+
+.pop-up-body {
+  min-height: 20vh;
+  background-color: purple;
 }
 
 .visibility {
