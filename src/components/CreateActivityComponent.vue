6a273d3b6736e865e06d332539faafd02949b1d6
LeottaAlberto <leottaalberto43@gmail.com>
2025-12-16 21:58:23 +0100

    feat(CreateActivityComponent): remove some elements from css, html and ts to adapt the component to the new mangament

--- src/components/CreateActivityComponent.vue
+++ src/components/CreateActivityComponent.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref } from 'vue'
+import { onMounted, onUnmounted, ref, watch } from 'vue'
 import type ActivityInterface from '@/interface/ActivityInterface'
 
 const optionStored = ref()
@@ -13,6 +13,15 @@ const duration = ref(0)
 const formRef = ref<HTMLFormElement | null>(null)
 
 const emit = defineEmits(['submit', 'closed'])
+const props = defineProps({
+  isSubmitClicked: Boolean,
+})
+
+watch(props, () => {
+  const btn = document.getElementById('submit-add-activity-btn')
+
+  if (btn) btn.click()
+})
 
 optionStored.value = JSON.parse(localStorage.getItem('priority-option') || '{}')
 
@@ -72,122 +81,73 @@ addEventListener('keypress', (key) => {
 </script>
 <template>
   <div class="create-activity-container flex">
-    <div class="flex field-container">
-      <form class="w-100 flex" ref="formRef" @submit.prevent="submit">
-        <h1 class="text-bolder">Add Activity</h1>
+    <form class="flex f-col w-100" ref="formRef" @submit.prevent="submit">
+      <div class="flex f-row g-1 w-100 min-w-100">
+        <div class="w-100">
+          <h3>Title *</h3>
+          <input name="title-content" type="text" v-model="title" required />
+        </div>
+        <div class="w-50">
+          <h3>Time (h)</h3>
+          <input
+            name="duration"
+            type="number"
+            placeholder="How much time in hours"
+            class="font-size-little"
+            v-model="duration"
+            required
+          />
+        </div>
+      </div>
 
-        <div class="flex f-row g-1 w-100 min-w-100">
-          <div class="w-100">
-            <h3>Title *</h3>
-            <input name="title-content" type="text" v-model="title" required />
-          </div>
-          <div class="w-50">
-            <h3>Time (h)</h3>
-            <input
-              name="duration"
-              type="number"
-              placeholder="How much time in hours"
-              class="font-size-little"
-              v-model="duration"
-              required
-            />
-          </div>
+      <div class="flex f-row g-1 w-100 min-w-100">
+        <div class="w-100">
+          <h3>Category *</h3>
+          <input
+            class="w-100 min-w-100"
+            name="category-content"
+            type="text"
+            v-model="category"
+            required
+          />
         </div>
 
-        <div class="flex f-row category-center w-100 min-w-100">
-          <div class="w-100">
-            <h3>Category *</h3>
-            <input
-              class="w-100 min-w-100"
-              name="category-content"
-              type="text"
-              v-model="category"
-              required
-            />
-          </div>
-
-          <div class="w-50">
-            <h3>Priority *</h3>
-            <select
-              v-if="optionsArray.length > 0"
-              class="w-100 btn select"
-              v-model="priority"
-              required
-            >
-              <option v-for="opt in optionsArray" :value="opt" :key="opt">
-                {{ opt }}
-              </option>
-            </select>
-            <select v-else class="w-100 btn select" v-model="priority">
-              <option>Lower</option>
-              <option>Medium</option>
-              <option>Higher</option>
-            </select>
-          </div>
+        <div class="w-50">
+          <h3>Priority *</h3>
+          <select
+            v-if="optionsArray.length > 0"
+            class="w-100 btn select"
+            v-model="priority"
+            required
+          >
+            <option v-for="opt in optionsArray" :value="opt" :key="opt">
+              {{ opt }}
+            </option>
+          </select>
+          <select v-else class="w-100 btn select" v-model="priority">
+            <option>Lower</option>
+            <option>Medium</option>
+            <option>Higher</option>
+          </select>
         </div>
-        <div class="txt-area-container w-100">
-          <h3>Note</h3>
-          <textarea
-            v-model="note"
-            id="note-content"
-            name="note-content"
-            class="txt-area"
-          ></textarea>
-        </div>
-        <p class="font-size-little">* campi obbligatori</p>
-        <div
-          class="w-100 px-2"
-          style="display: flex; justify-content: space-between; gap: 10vw; margin-top: 1vw"
-        >
-          <input type="button" value="Cancel" class="btn" @click="closeCreateActivity()" />
-          <input type="submit" value="Add" class="btn" />
-        </div>
-      </form>
-    </div>
+      </div>
+      <div class="txt-area-container w-100">
+        <h3>Note</h3>
+        <textarea v-model="note" id="note-content" name="note-content" class="txt-area"></textarea>
+      </div>
+      <p class="font-size-little">* campi obbligatori</p>
+      <input type="submit" id="submit-add-activity-btn" hidden />
+    </form>
   </div>
 </template>
 <style scoped>
-form {
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-
-.create-activity-container {
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  background-color: rgba(0, 0, 0, 0.7);
-  z-index: 999;
-  top: 0;
-  left: 0;
-}
-
 .create-activity-container input {
   border-radius: 7px;
   border: none;
-  height: 2.5vw;
+  height: 2vw;
   width: 100%;
   color: var(--color-text);
   padding-inline: 0.3vw;
-}
-
-.field-container {
-  width: 40%;
-  height: fit-content;
-  max-height: 60vh;
-  max-width: 100%;
-
-  background-color: #4d4d4dd8;
-
-  padding: 4vw;
-
-  flex-direction: column;
-  justify-content: left;
-  align-items: baseline;
-  border-radius: 15px;
-
-  gap: 1vh;
 }
 
 .txt-area {
@@ -203,15 +163,10 @@ form {
   width: 100%;
 }
 
-.category-center {
-  gap: 0.5vw;
-}
-
 .select {
-  border-radius: 8px;
   background-color: var(--color-background);
   color: var(--color-text);
-  height: 2.5vw;
   padding-inline: 0.5vw;
+  height: 2vw;
 }
 </style>
