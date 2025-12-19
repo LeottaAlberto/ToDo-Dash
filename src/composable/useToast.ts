04568c9721070acdde06a58d8900c1b7ea4be038
LeottaAlberto <leottaalberto43@gmail.com>
2025-12-19 16:36:43 +0100

    feat(useToast): create and export useToast function

---
+++ src/composable/useToast.ts
@@ -0,0 +1,19 @@
+import type { ToastMessage } from '@/core/interface/toast.interface'
+import { ref } from 'vue'
+
+const activeToast = ref<ToastMessage | null>(null)
+
+export function useToast() {
+  const showToast = (config: ToastMessage) => {
+    activeToast.value = config
+
+    setTimeout(() => {
+      activeToast.value = null
+    }, config.duration || 3000)
+  }
+
+  return {
+    activeToast,
+    showToast,
+  }
+}
