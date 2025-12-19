96e0023a4d6a26c1a236765e9399e9d75cd563c1
LeottaAlberto <leottaalberto43@gmail.com>
2025-12-19 16:30:49 +0100

    refactor(message.ts)

--- src/core/constants/messages.ts
+++ src/core/constants/messages.ts
@@ -1,14 +1,14 @@
-import { ToastType } from "../enum/toast.enum";
+import { ToastType } from '../enum/toast.enum'
 
 export const APP_MESSAGE = {
-  ACTIVITY : {
-    ADD_SUCCESS: { text: "Activity added with succesflully", type: ToastType.SUCCESS} ,
-    ADD_ERROR: { text: "Failed to add activity", type: ToastType.ERROR },
-    DELETE_WARNING: { text: "Activity deleted", type: ToastType.WARNING },
+  ACTIVITY: {
+    ADD_SUCCESS: { text: 'Activity added with succesflully', type: ToastType.SUCCESS },
+    ADD_ERROR: { text: 'Failed to add activity', type: ToastType.ERROR },
+    DELETE_WARNING: { text: 'Activity deleted', type: ToastType.WARNING },
   },
-  AUTH : {
-    LOGIN_SUCCESS: { text: "Login successful", type: ToastType.SUCCESS },
-    LOGIN_ERROR: { text: "Unable to log in", type: ToastType.ERROR },
+  AUTH: {
+    LOGIN_SUCCESS: { text: 'Login successful', type: ToastType.SUCCESS },
+    LOGIN_ERROR: { text: 'Unable to log in', type: ToastType.ERROR },
     LOGIN_WARNING: { text: "Something's wrong, try entering the data again", type: ToastType.ERROR },
-  }
-}
+  },
+} as const
