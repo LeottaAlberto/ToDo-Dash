31d474000de65c7ffc1fce2aad6a5be7f95bd655
LeottaAlberto <leottaalberto43@gmail.com>
2025-12-21 15:29:24 +0100

    feat(message): add complete success/error message

--- src/core/constants/messages.ts
+++ src/core/constants/messages.ts
@@ -5,6 +5,8 @@ export const APP_MESSAGE = {
     ADD_SUCCESS: { text: 'Activity added with succesflully', type: ToastType.SUCCESS },
     ADD_ERROR: { text: 'Failed to add activity', type: ToastType.ERROR },
     DELETE_WARNING: { text: 'Activity deleted', type: ToastType.WARNING },
+    COMPLETED_SUCCESS: { text: 'Successfully completed task' , type: ToastType.SUCCESS },
+    COMPLETED_ERROR: { text: 'Unable to complete task' , type: ToastType.ERROR },
   },
   AUTH: {
     LOGIN_SUCCESS: { text: 'Login successful', type: ToastType.SUCCESS },
