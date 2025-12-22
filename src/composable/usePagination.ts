90a7fa054d8379d6fdde7af8dbd91ab568c0f3b1
LeottaAlberto <leottaalberto43@gmail.com>
2025-12-22 23:35:22 +0100

    feat(usePagination): create and write usePagination

---
+++ src/composable/usePagination.ts
@@ -0,0 +1,24 @@
+import { computed, ref } from "vue"
+import { useActivity } from '@/composable/useActivity'
+
+const { totalActivities } = useActivity()
+
+export function usePagination(itemPerPage: number = 5) {
+  const currentPage = ref(1)
+
+  const totalPages = computed(() => Math.ceil(totalActivities.value / itemPerPage))
+  const startIndex = computed(() => (currentPage.value - 1) * totalActivities.value)
+  const endIndex = computed(() => startIndex.value + totalActivities.value)
+
+  const nextPage = () => { if (currentPage.value < totalPages.value) currentPage.value++ }
+  const prevPage = () => { if (currentPage.value > 1) currentPage.value-- }
+
+  return {
+    currentPage,
+    totalPages,
+    startIndex,
+    endIndex,
+    nextPage,
+    prevPage
+  }
+}
