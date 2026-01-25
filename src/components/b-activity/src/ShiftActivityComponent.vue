2df9fa987d4292c2db43ebd35940f9bf7cc5cc2a
LeottaAlberto <leottaalberto43@gmail.com>
2026-01-25 22:23:11 +0100

    feat(ShiftActivityComponent, PopUpComponent): replace inline SVGs with icon prop in ButtonComponent for consistency

--- src/components/b-activity/src/ShiftActivityComponent.vue
+++ src/components/b-activity/src/ShiftActivityComponent.vue
@@ -10,56 +10,11 @@ const { nextPage, prevPage } = usePagination();
 <template>
   <div class="flex scroll-buttons w-50" v-if="allActivities.length > 5">
     <div class="flex border-standard w-25 radius-standard">
-      <ButtonComponent :is_svg="true" @click="prevPage()">
-        <svg
-          width="48px"
-          height="48px"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          transform="rotate(180)"
-        >
-          <g id="SVGRepo_bgCarrier" stroke-width="0" />
-          <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
-          <g id="SVGRepo_iconCarrier">
-            <path
-              d="M9 5L14.15 10C14.4237 10.2563 14.6419 10.5659 14.791 10.9099C14.9402 11.2539 15.0171 11.625 15.0171 12C15.0171 12.375 14.9402 12.7458 14.791 13.0898C14.6419 13.4339 14.4237 13.7437 14.15 14L9 19"
-              stroke="#181818"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              data-darkreader-inline-stroke=""
-              style="--darkreader-inline-stroke: var(--darkreader-text-000000, #e8e6e3)"
-            />
-          </g>
-        </svg>
-      </ButtonComponent>
+      <ButtonComponent :icon="'leftArrow'" @click="prevPage()" />
 
       <h2 class="text-bolder text-align-center">Activity</h2>
 
-      <ButtonComponent :is_svg="true" @click="nextPage()">
-        <svg
-          width="48px"
-          height="48px"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <g id="SVGRepo_bgCarrier" stroke-width="0" />
-          <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
-          <g id="SVGRepo_iconCarrier">
-            <path
-              d="M9 5L14.15 10C14.4237 10.2563 14.6419 10.5659 14.791 10.9099C14.9402 11.2539 15.0171 11.625 15.0171 12C15.0171 12.375 14.9402 12.7458 14.791 13.0898C14.6419 13.4339 14.4237 13.7437 14.15 14L9 19"
-              stroke="#181818"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              data-darkreader-inline-stroke=""
-              style="--darkreader-inline-stroke: var(--darkreader-text-000000, #e8e6e3)"
-            />
-          </g>
-        </svg>
-      </ButtonComponent>
+      <ButtonComponent :icon="'rightArrow'" @click="nextPage()" />
     </div>
   </div>
 </template>
