b38170f2a04c31b63470d7c26b396a111610a2ba
LeottaAlberto <leottaalberto43@gmail.com>
2026-02-02 21:23:08 +0100

    style(TitleComponent.vue): refactor layout and styles with tailwind classes

--- src/components/b-title/src/TitleComponent.vue
+++ src/components/b-title/src/TitleComponent.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="title-component-container flex box-shadow">
-    <div class="flex">
-      <h1 class="text-bolder font-size-big">My ToDo-Dash</h1>
+  <div class="title-component-container flex w-75 h-20 z-10">
+    <div
+      class="flex flex-row justify-between items-center rounded-md w-full h-max py-2 px-4 outline-2 outline-neutral-700 bg-transparent"
+    >
+      <h1 class="text-bolder text-4xl font-bold">My ToDo-Dash</h1>
       <ButtonComponent @click="emit('clicked')"> Add Activity </ButtonComponent>
-      <!-- <input type="button" value="Add Activity" @click="addActivityClicked()" class="btn" /> -->
     </div>
   </div>
 </template>
@@ -13,32 +14,3 @@ import ButtonComponent from '@/components/b-utility/src/ButtonComponent.vue';
 
 const emit = defineEmits(['clicked']);
 </script>
-
-<style scoped>
-.title-component-container {
-  width: 80%;
-  height: 20%;
-  z-index: 1;
-}
-
-.title-component-container > div {
-  width: 100%;
-  height: max-content;
-  padding: 0.5vw;
-  padding-inline: 2vw;
-  flex-direction: row;
-  background-color: transparent;
-  border: 2px solid #4d4d4d;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border-radius: 10px;
-}
-
-.btn {
-  width: 12%;
-  height: 4vh;
-  border-radius: 5px;
-  color: var(--text-color);
-}
-</style>
