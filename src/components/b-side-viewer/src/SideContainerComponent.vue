293549228d9498a968f9042dfa6f2fb3b05c41d0
LeottaAlberto <leottaalberto43@gmail.com>
2026-02-03 23:18:04 +0100

    style(SideContainerComponent.vue): refactor layout and styles using tailwind classes

--- src/components/b-side-viewer/src/SideContainerComponent.vue
+++ src/components/b-side-viewer/src/SideContainerComponent.vue
@@ -5,8 +5,8 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div class="flex flex-col w-65">
-    <h1 class="w-full text-align-center text-bolder text-md-big">{{ props.title }}</h1>
+  <div class="flex flex-col gap-2 w-65">
+    <h1 class="w-full text-center font-bold text-4xl">{{ props.title }}</h1>
     <div
       class="w-full h-full py-2 px-1 rounded-md shadow-lg shadow-sky-100/10 outline-2 outline-neutral-700"
     >
