38ff41941fcc561c59bd00bbbd700cff5afec5db
LeottaAlberto <leottaalberto43@gmail.com>
2026-02-08 21:25:33 +0100

    style(App.vue): replace old css classes with tailwind classes

--- src/App.vue
+++ src/App.vue
@@ -41,15 +41,9 @@ function saveData() {
   -->
 
   <main>
-    <RouterView></RouterView>
-    <ToastComponent />
+    <div class="w-full h-screen justify-center items-center">
+      <RouterView></RouterView>
+      <ToastComponent />
+    </div>
   </main>
 </template>
-
-<style scoped>
-main {
-  max-width: 100vw;
-  max-height: 100vh;
-  overflow-x: hidden;
-}
-</style>
