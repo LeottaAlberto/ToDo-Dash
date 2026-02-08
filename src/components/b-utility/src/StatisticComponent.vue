3358e8edce59dd500210a0b518b2cad0e949c5d0
LeottaAlberto <leottaalberto43@gmail.com>
2026-02-08 21:44:07 +0100

    style(StatisticComponent.vue): remake html structure. Replace slot e component with tailwind classes

--- src/components/b-utility/src/StatisticComponent.vue
+++ src/components/b-utility/src/StatisticComponent.vue
@@ -1,35 +1,21 @@
 <script lang="ts" setup>
-import { computed, defineAsyncComponent } from 'vue';
-
 const props = defineProps({
   statName: String,
   iconName: String,
   statNum: Number,
 });
-
-const iconComponent = computed(() => {
-  if (!props.iconName) return null;
-  return defineAsyncComponent(() => import(`@/assets/SVGs/statistics/${props.iconName}.svg`));
-});
 </script>
 
 <template>
-  <div class="flex items-center justify-between w-full rounded-md gap-2 px-2 py-4">
+  <div class="flex items-center justify-between w-full rounded-md gap-2 p-2">
     <div class="flex justify-center-safe items-center-safe w-50 max-w-15 h-15">
-      <component
-        v-if="iconComponent"
-        :is="iconComponent"
-        class="flex justify-center items-center w-full h-full min-y-10"
-      />
-      <div v-else class="flex justify-center items-center w-full h-full min-y-10">
-        <slot></slot>
-      </div>
+      <div class="min-y-10 pi text-4xl text-center" :class="props.iconName"></div>
     </div>
 
-    <div class="flex grow items-center">
+    <div class="flex grow items-center justify-start">
       <h4>{{ statName }}</h4>
     </div>
 
-    <h1 class="font-extrabold text-2xl min-y-10 text-right m-0 text-stone-300">{{ statNum }}</h1>
+    <h1 class="font-bold text-2xl min-y-10 text-right m-0 text-stone-300">{{ statNum }}</h1>
   </div>
 </template>
