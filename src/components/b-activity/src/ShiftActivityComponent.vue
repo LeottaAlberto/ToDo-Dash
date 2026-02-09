d465de2af3d32e2cd7d31fc528cfe3a408119b07
LeottaAlberto <leottaalberto43@gmail.com>
2026-02-09 21:11:53 +0100

    feat(ShiftActivityComponent.vue): replace svgs name with icon class and manage the component's dimension

--- src/components/b-activity/src/ShiftActivityComponent.vue
+++ src/components/b-activity/src/ShiftActivityComponent.vue
@@ -4,17 +4,17 @@ import { useActivity } from '@/composable/useActivity';
 import { usePagination } from '@/composable/usePagination';
 
 const { allActivities } = useActivity();
-const { nextPage, prevPage } = usePagination();
+const { nextPage, prevPage, currentPage } = usePagination();
 </script>
 
 <template>
-  <div class="absolute bottom-0 w-full flex justify-center py-4" v-if="allActivities.length > 5">
-    <div class="bg-neutral-800 flex justify-between px-2 w-2/12 rounded-xl">
-      <ButtonComponent :icon="'leftArrow'" @click="prevPage()" />
+  <div class="w-full flex justify-center" v-if="allActivities.length > 5">
+    <div class="bg-neutral-800 flex justify-between px-2 w-1/2 rounded-xl">
+      <ButtonComponent :icon="'pi-angle-left'" @click="prevPage()" />
 
-      <h2 class="text-center flex items-center font-bold text-3xl">Activity</h2>
+      <h2 class="text-center flex items-center font-bold text-3xl">Page {{ currentPage }}</h2>
 
-      <ButtonComponent :icon="'rightArrow'" @click="nextPage()" />
+      <ButtonComponent :icon="'pi-angle-right'" @click="nextPage()" />
     </div>
   </div>
 </template>
