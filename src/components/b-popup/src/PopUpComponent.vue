f0435589a16b31129258ea59b472602f74ddae55
LeottaAlberto <leottaalberto43@gmail.com>
2026-02-09 20:57:13 +0100

    feat(PopUpComponent.vue): improve popup. Now you can open it more smoothly and his height better fits. Add Popup message warning

--- src/components/b-popup/src/PopUpComponent.vue
+++ src/components/b-popup/src/PopUpComponent.vue
@@ -1,86 +1,94 @@
 <script lang="ts" setup>
 import type ActivityInterface from '@/core/interface/ActivityInterface';
-import { APP_MESSAGE } from '@/core/constants/messages';
-import { useToast } from '@/composable/useToast';
-import { useActivity } from '@/composable/useActivity';
 import ButtonComponent from '@/components/b-utility/src/ButtonComponent.vue';
-import { Position } from '@/enums';
+import { computed, onMounted, onUnmounted, useSlots } from 'vue';
 
 const props = defineProps<{
   title: string;
   footer_btn_title: string;
   disable_btn_footer: boolean | true;
   activity?: ActivityInterface;
+  icon_button_2?: string;
 }>();
 
-const { showToast } = useToast();
-const { completeActivity } = useActivity();
-const emits = defineEmits(['closed']);
+const slots = useSlots();
+
+const emits = defineEmits(['closed', 'confirm']);
 
 function closePopUp() {
   emits('closed');
 }
 
-function complete() {
-  try {
-    if (!props.activity) throw new Error('');
-
-    completeActivity(props.activity);
-    closePopUp();
-    showToast(APP_MESSAGE.ACTIVITY.COMPLETED_SUCCESS);
-  } catch (e) {
-    showToast(APP_MESSAGE.ACTIVITY.COMPLETED_ERROR);
-    console.error(e);
-  }
+function clickBtn2() {
+  emits('confirm', props.activity);
 }
+
+const handleKeyDown = (e: KeyboardEvent) => {
+  if (e.key === 'Escape') closePopUp();
+};
+
+/**
+ * @return true if slot is string
+ * @return false if slot is something other than the string
+ */
+const slotType = computed(() => {
+  const content = slots.default?.();
+
+  if (!content) return false;
+
+  const firstNode = content[0];
+
+  if (typeof firstNode?.children === 'string') return true;
+
+  return false;
+});
+
+const bodyClass = computed(() => {
+  return [slotType.value ? 'h-20 max-h-1/12 text-2xl p-2' : 'min-h-1/2 max-h-8/12'];
+});
+
+onMounted(() => window.addEventListener('keydown', handleKeyDown));
+onUnmounted(() => window.removeEventListener('keydown', handleKeyDown));
 </script>
 
 <template>
   <div
-    class="flex justify-center items-center top-0 left-0 p-0 m-0 z-9999 absolute w-full h-full bg-black/20"
+    tabindex="1"
+    class="flex justify-center items-center top-0 left-0 p-0 m-0 z-40 absolute w-full h-full bg-black/20"
+    @click.self="closePopUp()"
   >
     <div
-      class="flex flex-col px-5 py-3 max-h-4/6 max-w-2/5 absolute rounded-xl w-full h-fit min-h-1/2 bg-zinc-900"
+      class="flex flex-col px-10 py-7 z-50 max-h-4/6 max-w-2/5 inset-0 rounded-xl w-full h-fit bg-zinc-900"
+      tabindex="0"
+      @keypress.esc="closePopUp()"
     >
       <!-- Header -->
-      <div class="flex just-content-space-between w-full">
-        <div class="flex flex-row basis-2/3">
+      <div class="flex just-content-space-between w-full py-1">
+        <div class="flex flex-row w-full">
           <h1
             class="flex justify-start items-center text-center px-2 pt-1 text-5xl font-bold w-full"
           >
             {{ props.title }}
           </h1>
         </div>
-
-        <div class="flex basis-1/3 px-2">
-          <ButtonComponent
-            :is_svg="true"
-            :direction="Position.RIGHT"
-            :icon="'close'"
-            class="w-full"
-            @click="closePopUp()"
-          >
-          </ButtonComponent>
-        </div>
       </div>
 
       <!-- Body -->
-      <div class="flex-1 overflow-y-hidden w-full min-h-1/2 max-h-8/12">
+      <div class="overflow-y-hidden w-full py-2" :class="[bodyClass]">
         <slot />
       </div>
 
       <!-- Footer -->
       <div class="w-full p-4">
         <slot name="footer" v-if="$slots.footer" />
-        <div class="flex justify-end w-full" v-else>
+        <div class="flex flex-row justify-end gap-3" v-else>
+          <ButtonComponent :icon="'pi-times'" :label="'Close'" @click="closePopUp()" />
           <ButtonComponent
-            :direction="Position.RIGHT"
             :disable="props.disable_btn_footer"
-            class="w-full"
-            @click="complete()"
-          >
-            {{ props.footer_btn_title }}
-          </ButtonComponent>
+            :label="props.footer_btn_title"
+            :icon="props.icon_button_2 ? props.icon_button_2 : undefined"
+            @click="clickBtn2()"
+          />
         </div>
       </div>
     </div>
