8bda82fd277db7dee350da27179c0fcb727d1737
LeottaAlberto <leottaalberto43@gmail.com>
2026-02-09 20:49:51 +0100

    refactor(usePagination.ts): add global itemPerPage

--- src/composable/usePagination.ts
+++ src/composable/usePagination.ts
@@ -3,9 +3,10 @@ import { useActivity } from '@/composable/useActivity';
 
 const { totalActivities } = useActivity();
 const currentPage = ref(1);
+const itemPerPage = ref(-1);
 
-export function usePagination(itemPerPage: number = 5) {
-  const totalPages = computed(() => Math.ceil(totalActivities.value / itemPerPage));
+export function usePagination(item: number = 5) {
+  const totalPages = computed(() => Math.ceil(totalActivities.value / item));
   const startIndex = computed(() => (currentPage.value - 1) * totalActivities.value);
   const endIndex = computed(() => startIndex.value + totalActivities.value);
 
