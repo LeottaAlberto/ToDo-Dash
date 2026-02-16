163cf987215b1660aad3553a475c2f22c3cf526d
LeottaAlberto <leottaalberto43@gmail.com>
2026-02-16 12:48:31 +0100

    refactor(ShiftActivityComponent.vue): refactor and change tailwind classes

--- src/components/b-activity/src/ShiftActivityComponent.vue
+++ src/components/b-activity/src/ShiftActivityComponent.vue
@@ -2,19 +2,20 @@
 import ButtonComponent from '@/components/b-utility/src/ButtonComponent.vue';
 import { useActivity } from '@/composable/useActivity';
 import { usePagination } from '@/composable/usePagination';
+import { ButtonDimension } from '@/enums/ButtonEnum';
 
 const { allActivities } = useActivity();
 const { nextPage, prevPage, currentPage } = usePagination();
 </script>
 
 <template>
-  <div class="w-full flex justify-center" v-if="allActivities.length > 5">
-    <div class="bg-neutral-800 flex justify-between px-2 w-1/2 rounded-xl">
-      <ButtonComponent :icon="'pi-angle-left'" @click="prevPage()" />
+  <div class="w-1/2 flex justify-center" v-if="allActivities.length > 5">
+    <div class="flex justify-between px-2 w-1/2 rounded-xl">
+      <ButtonComponent icon="pi-angle-left" :dimension="ButtonDimension.BIG" @click="prevPage()" />
 
-      <h2 class="text-center flex items-center font-bold text-3xl">Page {{ currentPage }}</h2>
+      <h2 class="text-center flex items-center font-semibold text-2xl">Page {{ currentPage }}</h2>
 
-      <ButtonComponent :icon="'pi-angle-right'" @click="nextPage()" />
+      <ButtonComponent icon="pi-angle-right" :dimension="ButtonDimension.BIG" @click="nextPage()" />
     </div>
   </div>
 </template>
