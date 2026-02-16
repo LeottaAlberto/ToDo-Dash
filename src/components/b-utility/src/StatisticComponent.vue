a4690eb94107194c58b99148ceb68421c6b42ce5
LeottaAlberto <leottaalberto43@gmail.com>
2026-02-16 12:27:20 +0100

    refactor(StatisticComponent.vue): changed the appearance to look like cards

--- src/components/b-utility/src/StatisticComponent.vue
+++ src/components/b-utility/src/StatisticComponent.vue
@@ -7,15 +7,14 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="flex items-center justify-between w-full rounded-md gap-2 p-2">
-    <div class="flex justify-center-safe items-center-safe w-50 max-w-15 h-15">
-      <div class="min-y-10 pi text-4xl text-center" :class="props.iconName"></div>
+  <div class="flex items-start justify-start w-full rounded-xl gap-2 p-2 bg-zinc-950/40">
+    <div class="flex justify-center-safe items-center-safe w-1/4 max-w-15 h-15">
+      <div class="min-y-10 pi text-4xl! text-center" :class="props.iconName"></div>
     </div>
 
-    <div class="flex grow items-center justify-start">
+    <div class="flex flex-col flex-wrap items-center justify-start w-2/3">
       <h4>{{ statName }}</h4>
+      <h1 class="font-bold text-2xl min-y-10 text-right m-0 text-stone-300">{{ statNum }}</h1>
     </div>
-
-    <h1 class="font-bold text-2xl min-y-10 text-right m-0 text-stone-300">{{ statNum }}</h1>
   </div>
 </template>
