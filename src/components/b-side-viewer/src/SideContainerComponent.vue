bb7eb73864dc4bc11cbd011457e7310652313c26
LeottaAlberto <leottaalberto43@gmail.com>
2026-02-16 12:29:50 +0100

    refactor(SideContainerComponent.vue): changed tailwind classes and removed unused code

--- src/components/b-side-viewer/src/SideContainerComponent.vue
+++ src/components/b-side-viewer/src/SideContainerComponent.vue
@@ -1,19 +1,14 @@
-<script setup lang="ts">
-// const props = defineProps<{
-//   title: string;
-// }>();
-// bg-purple-950
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div
-    class="flex flex-col justify-between items-center gap-2 w-1/5 h-full my-10 rounded-2xl bg-zinc-800"
+    class="flex flex-col justify-between items-center gap-2 w-1/5 h-full my-10 rounded-2xl bg-zinc-800/50"
   >
-    <header class="flex flex-col justify-center items-center w-full">
+    <header class="flex flex-col justify-center items-center w-full p-5">
       <slot name="header" />
     </header>
 
-    <div class="flex flex-col justify-center items-center w-full h-3/4">
+    <div class="flex flex-col justify-baseline items-center w-full h-3/4">
       <slot name="body" />
     </div>
 
