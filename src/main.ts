07e0256c21517dfcb444f503e21cb5a1326f1caf
LeottaAlberto <leottaalberto43@gmail.com>
2026-02-17 11:44:09 +0100

    Install primevue themes

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,26 @@ import 'primeicons/primeicons.css';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+
 import App from './App.vue';
 import router from './router';
 
 const app = createApp(App);
 
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: '.my-app-dark', // o 'system'
+      cssLayer: {
+        name: 'primevue',
+        order: 'tailwind-base, primevue, tailwind-utilities',
+      },
+    },
+  },
+});
 app.use(createPinia());
 app.use(router);
 
