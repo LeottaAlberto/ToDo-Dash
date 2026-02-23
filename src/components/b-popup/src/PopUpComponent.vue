c7a7d29d0d1561efe0910a47ab1468474830dff3
LeottaAlberto <leottaalberto43@gmail.com>
2026-02-23 23:47:29 +0100

    feat(PopUpComponent.vue): add transition and tooltip on footer buttons

--- src/components/b-popup/src/PopUpComponent.vue
+++ src/components/b-popup/src/PopUpComponent.vue
@@ -5,6 +5,7 @@ import { computed, onMounted, onUnmounted, useSlots } from 'vue';
 import { ButtonDimension, ButtonRadius } from '@/enums/ButtonEnum';
 
 const props = defineProps<{
+  show: boolean | false;
   title: string;
   footer_btn_title: string;
   disable_btn_footer?: boolean | true;
@@ -57,61 +58,80 @@ onUnmounted(() => window.removeEventListener('keydown', handleKeyDown));
 </script>
 
 <template>
-  <div
-    tabindex="1"
-    class="flex justify-center items-center top-0 left-0 p-0 m-0 z-50 absolute w-full h-full bg-black/20"
-    @click.self="closePopUp()"
+  <Transition
+    enter-active-class="duration-75 ease-out"
+    enter-from-class="transform scale-95 opacity-0"
+    enter-to-class="transform scale-100 opacity-100"
+    leave-active-class="duration-75 ease-in"
+    leave-from-class="transform scale-100 opacity-100"
+    leave-to-class="transform scale-95 opacity-0"
   >
     <div
-      class="flex flex-col z-50 max-h-4/6 max-w-2/5 inset-0 rounded-xl overflow-hidden w-fit h-fit bg-zinc-900"
-      tabindex="0"
-      @keypress.esc="closePopUp()"
+      v-if="props.show"
+      class="flex justify-center items-center top-0 left-0 p-0 m-0 z-50 absolute w-full h-full bg-black/20 trnasition"
+      @click.self="closePopUp()"
     >
-      <!-- Header -->
-      <div class="flex flex-col justify-between w-full bg-neutral-600 relative">
-        <div
-          v-if="props.editable"
-          class="flex flex-row justify-end gap-1 text-3xl absolute right-1 top-1"
-        >
-          <ButtonComponent :background="true" icon="pi-pencil" @click="emits('edit')" />
-          <ButtonComponent :background="true" icon="pi-trash" @click="emits('remove')" />
+      <div
+        class="flex flex-col z-50 max-h-4/6 max-w-2/5 inset-0 rounded-xl overflow-hidden w-fit h-fit bg-zinc-900"
+        tabindex="0"
+        @keypress.esc="closePopUp()"
+      >
+        <!-- Header -->
+        <div class="flex flex-col justify-between w-full bg-neutral-600 relative">
+          <div
+            v-if="props.editable"
+            class="flex flex-row justify-end gap-1 text-3xl absolute right-2 top-2"
+          >
+            <ButtonComponent
+              :background="true"
+              tool-tip="Edit"
+              icon="pi-pencil"
+              @click="emits('edit')"
+            />
+            <ButtonComponent
+              :background="true"
+              tool-tip="Remove"
+              icon="pi-trash"
+              @click="emits('remove')"
+            />
+          </div>
+          <div class="flex flex-row">
+            <h1 class="text-start p-6 text-4xl font-bold w-fit text-nowrap">
+              {{ props.title }}
+            </h1>
+            <div class="flex justify-start items-center w-full">
+              <slot name="title-element"></slot>
+            </div>
+          </div>
         </div>
-        <div class="flex flex-row">
-          <h1 class="text-start p-6 text-4xl font-bold w-fit text-nowrap">
-            {{ props.title }}
-          </h1>
-          <div class="flex justify-start items-center w-full">
-            <slot name="title-element"></slot>
+
+        <!-- Body -->
+        <div class="w-full px-5 py-5" :class="[bodyClass]">
+          <slot />
+        </div>
+
+        <!-- Footer -->
+        <div class="w-full py-6 px-10">
+          <slot name="footer" v-if="$slots.footer" />
+          <div class="flex flex-row justify-end gap-3" v-else>
+            <ButtonComponent
+              :icon="'pi-times'"
+              :dimension="ButtonDimension.SMALL"
+              :label="'Close'"
+              :radius="ButtonRadius.SMALL"
+              @click="closePopUp()"
+            />
+            <ButtonComponent
+              :disable="props.disable_btn_footer"
+              :label="props.footer_btn_title"
+              :dimension="ButtonDimension.SMALL"
+              :icon="props.icon_button_2 ? props.icon_button_2 : undefined"
+              :radius="ButtonRadius.SMALL"
+              @click="clickBtn2()"
+            />
           </div>
         </div>
       </div>
-
-      <!-- Body -->
-      <div class="w-full px-5 py-5" :class="[bodyClass]">
-        <slot />
-      </div>
-
-      <!-- Footer -->
-      <div class="w-full py-6 px-10">
-        <slot name="footer" v-if="$slots.footer" />
-        <div class="flex flex-row justify-end gap-3" v-else>
-          <ButtonComponent
-            :icon="'pi-times'"
-            :dimension="ButtonDimension.SMALL"
-            :label="'Close'"
-            :radius="ButtonRadius.SMALL"
-            @click="closePopUp()"
-          />
-          <ButtonComponent
-            :disable="props.disable_btn_footer"
-            :label="props.footer_btn_title"
-            :dimension="ButtonDimension.SMALL"
-            :icon="props.icon_button_2 ? props.icon_button_2 : undefined"
-            :radius="ButtonRadius.SMALL"
-            @click="clickBtn2()"
-          />
-        </div>
-      </div>
     </div>
-  </div>
+  </Transition>
 </template>
