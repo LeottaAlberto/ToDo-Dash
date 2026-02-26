6cd3c23ecb9f4879e5bc884ade5590ddb8dffe9d
LeottaAlberto <leottaalberto43@gmail.com>
2026-02-26 10:48:27 +0100

    feat(RightContainerComponent.vue): fix 'new activity' button's position

--- src/components/b-side-viewer/src/RightContainerComponent.vue
+++ src/components/b-side-viewer/src/RightContainerComponent.vue
@@ -12,13 +12,15 @@ const props = defineEmits(['isVisible']);
   <div class="w-fit min-w-1/5 h-full p-4">
     <SideContainerComponent title="Overview">
       <template v-slot:header>
-        <ButtonComponent
-          :direction="Position.RIGHT"
-          :icon="'pi-plus'"
-          :label="'New Activity'"
-          :radius="ButtonRadius.SMALL"
-          @click="props('isVisible', true)"
-        />
+        <div class="flex justify-center items-center w-full">
+          <ButtonComponent
+            :direction="Position.CENTER"
+            :icon="'pi-plus'"
+            :label="'New Activity'"
+            :radius="ButtonRadius.SMALL"
+            @click="props('isVisible', true)"
+          />
+        </div>
       </template>
       <template v-slot:body>
         <DashboardComponent :activities="allActivities" />
